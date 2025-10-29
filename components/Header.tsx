e304b075a9e91b0abd70b6638abce516fff6ffa9
Jerry <jerytom33@gmail.com>
2025-10-29 22:15:50 +0530

    Almost final UI done

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ export const Header: React.FC = () => {
     <>
       <header className="sticky top-0 z-50 py-3 px-4 md:px-8 bg-[#01010c]/80 backdrop-blur-sm border-b border-gray-800/50">
         <div className="container mx-auto flex justify-between items-center">
-          <img src="/assets/Fairgo_logo.png" alt="FAIRGO Logo" className="h-8 w-auto" />
+          <img src="../assets/Fairgo_logo.png" alt="FAIRGO Logo" className="h-4 w-auto" />
           <nav className="hidden md:flex items-center space-x-8">
             {navLinks.map((item) => (
               <a key={item} href="#" className="animated-underline text-gray-400 hover:text-white transition-colors duration-300">{item}</a>
